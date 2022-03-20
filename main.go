50a8961e9a03f7bc7472e9b17e7b1126cce760ba
Ayush <ayushkushwaha321@gmail.com>
2022-03-20 15:32:58 +0530

    Assignments Added

--- main.go
+++ main.go
@@ -28,8 +28,8 @@ func indexRoute(res *fiber.Ctx) error {
 	curr.All(context.Background(), &result)
 	json, _ := json.Marshal(result)
 	return res.Status(200).Send(json)
-
 }
+
 func addPerson(res *fiber.Ctx) error {
 	collection, err := getMongoDbCollection(dbname, collectionName)
 	if err != nil {
@@ -86,12 +86,69 @@ func deletePerson(res *fiber.Ctx) error {
 	return res.Status(200).Send(response)
 }
 
+func createPerson(res *fiber.Ctx) error {
+	collection, err := getMongoDbCollection(dbname, collectionName)
+	if err != nil {
+		return res.Status(400).SendString("There is some Problem! Please try again")
+	}
+	var newAssignment Assignment
+	json.Unmarshal([]byte(res.Body()), &newAssignment)
+	personCollection, err := getMongoDbCollection(dbname, collectionName)
+	if err != nil {
+		return res.Status(400).SendString("There is some Problem! Please try again")
+	}
+
+	id := res.Params("id")
+	newAssignment.Person = id
+	objID, _ := primitive.ObjectIDFromHex(id)
+	curr, _ := collection.InsertOne(context.Background(), newAssignment)
+	var filter bson.M = bson.M{"_id": objID}
+	var temp Person
+	personCollection.FindOne(context.Background(), filter).Decode(&temp)
+	temp.Assignment = append(temp.Assignment, curr.InsertedID.(primitive.ObjectID).Hex())
+
+	update := bson.M{
+		"$set": temp,
+	}
+
+	result, err := personCollection.UpdateOne(context.Background(), filter, update)
+
+	if err != nil {
+		return res.Status(400).SendString("There is some Problem! Please try again")
+	}
+	response, _ := json.Marshal(result)
+	return res.Status(200).Send(response)
+}
+
+func getAssignment(res *fiber.Ctx) error {
+	collection, err := getMongoDbCollection(dbname, collectionName)
+	if err != nil {
+		return res.Status(400).SendString("There is some Problem! Please try again")
+	}
+	id := res.Params("id")
+	var filter bson.M = bson.M{
+		"person": id,
+	}
+	curr, err := collection.Find(context.Background(), filter)
+	if err != nil {
+		return res.Status(400).SendString("There is some Problem! Please try again")
+	}
+	defer curr.Close(context.Background())
+	var result []bson.M
+	curr.All(context.Background(), &result)
+	json, _ := json.Marshal(result)
+	return res.Status(200).Send(json)
+}
+
 func main() {
 	app := fiber.New()
 	app.Get("/", indexRoute)
 	app.Post("/create", addPerson)
 	app.Put("/update/:id", updatePerson)
 	app.Delete("/delete/:id", deletePerson)
+
+	app.Post("/assignment/:id", createPerson)
+	app.Get("/assignment/:id", getAssignment)
 
 	app.Listen(":3000")
 }
--- schema.go
+++ schema.go
@@ -1,7 +1,15 @@
 package main
 
 type Person struct {
-	_id         string `json:"id,omitempty"`
-	Name        string `json:"name,omitempty"`
-	Designation string `json:"designation,omitempty"`
+	_id         string   `json:"id,omitempty"`
+	Name        string   `json:"name,omitempty"`
+	Designation string   `json:"designation,omitempty"`
+	Assignment  []string `json:"assignment,omitempty"`
+}
+
+type Assignment struct {
+	_id    string `json:"id,omitempty"`
+	Title  string `json:"title,omitempty"`
+	Tasks  string `json:"tasks,omitempty"`
+	Person string `json:"person,omitempty"`
 }
